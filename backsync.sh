766f16694f723bb61ead028acd15f1c53988038a
davood-dorostkar <dado2hacker@gmail.com>
2024-08-12 11:16:05 +0330

    fix: file sync issues

--- backsync.sh
+++ backsync.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-package="$1"
-clean_path="${package%/}"
-rsync -aP /ros2_ws/codes/$clean_path /ros2_ws/src/
-
 search_path=/ros2_ws/codes
 all_dirs=$(find "$search_path" -maxdepth 1 -type d)
+filtered_dirs=$(echo "$all_dirs" | grep -v "^$search_path$")
 
-for dir in $all_dirs; do
+for dir in $filtered_dirs; do
     rsync -aP "$dir" /ros2_ws/src/
 done
--- sync.sh
+++ sync.sh
@@ -7,7 +7,6 @@ exclude_dirs=".git rosbot_xl_controller rosbot_hardware_interfaces ros_component
 all_dirs=$(find "$search_path" -maxdepth 1 -type d)
 exclude_dirs="$exclude_dirs $(basename "$search_path")"
 filtered_dirs=$(echo "$all_dirs" | grep -vE "$(echo "$exclude_dirs" | sed 's/ /|/g')")
-echo "$filtered_dirs"
 
 for dir in $filtered_dirs; do
     rsync -aP "$dir" /ros2_ws/codes/
